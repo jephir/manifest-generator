8778384bf086ea739403e36c254c5b63b15330ff
Jephir <jephir@jephir.name>
2012-09-14 10:59:49 -0400

    Initial commit

---
+++ ManifestGenerator/Manifest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Test.CommandLineParsing;
+
+namespace ManifestGenerator
+{
+    class Manifest : Command
+    {
+        static SHA256 sha256 = new SHA256Managed();
+
+        public string ContentDirectory { get; set; }
+        public string ContentServerUri { get; set; }
+        public string OutputFile { get; set; }
+        public string InstallerPath { get; set; }
+        public string ApplicationPath { get; set; }
+        public string ApplicationArguments { get; set; }
+        public bool? IncludeManifest { get; set; }
+
+        public override void Execute()
+        {
+            SetDefaults();
+            CreateManifest();
+        }
+
+        void SetDefaults()
+        {
+            ContentDirectory = ContentDirectory ?? Directory.GetCurrentDirectory();
+            ContentServerUri = ContentServerUri ?? "http://localhost/";
+            OutputFile = OutputFile ?? Path.Combine(ContentDirectory, "manifest.xml");
+        }
+
+        void CreateManifest()
+        {
+            var contentDirectory = new DirectoryInfo(ContentDirectory);
+            var files = contentDirectory.GetFiles("*", SearchOption.AllDirectories);
+            var document = new XDocument(new XDeclaration("1.0", Encoding.UTF8.HeaderName, String.Empty));
+            var package = new XElement("package");
+
+            if (InstallerPath != null)
+            {
+                var installer = new XElement("installer");
+                installer.Add(new XAttribute("path", InstallerPath));
+                package.Add(installer);
+            }
+
+            if (ApplicationPath != null)
+            {
+                var application = new XElement("application");
+                application.Add(new XAttribute("path", ApplicationPath));
+                if (ApplicationArguments != null) application.Add(new XAttribute("arguments", ApplicationArguments));
+                package.Add(application);
+            }
+
+            foreach (var file in files)
+            {
+                if (file.Name.Equals(Path.GetFileName(OutputFile)) && IncludeManifest != true) continue;
+
+                var item = new XElement("item");
+
+                var filePath = Program.MakeRelativePath(contentDirectory.FullName + Path.DirectorySeparatorChar, file.FullName);
+                var fileUri = ContentServerUri + filePath;
+                var fileSize = file.Length;
+
+                var fileBytes = File.ReadAllBytes(file.FullName);
+                var fileHash = sha256.ComputeHash(fileBytes);
+                var fileHashString = BitConverter.ToString(fileHash).Replace("-", String.Empty).ToLower();
+
+                item.Add(new XAttribute("path", filePath));
+                item.Add(new XAttribute("uri", fileUri));
+                item.Add(new XAttribute("size", fileSize));
+                item.Add(new XAttribute("sha256", fileHashString));
+
+                package.Add(item);
+            }
+
+            document.Add(package);
+
+            using (var writer = new XmlTextWriter(OutputFile, new UTF8Encoding(false)))
+            {
+                writer.Formatting = Formatting.Indented;
+                document.Save(writer);
+            }
+        }
+    }
+}
---
+++ ManifestGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Test.CommandLineParsing;
+
+namespace ManifestGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manifest = new Manifest();
+
+            manifest.ParseArguments(args);
+            manifest.Execute();
+        }
+
+        // Modified from: http://stackoverflow.com/a/340454/238687
+        public static String MakeRelativePath(String fromPath, String toPath)
+        {
+            if (String.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
+            if (String.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
+
+            var fromUri = new Uri(fromPath);
+            var toUri = new Uri(toPath);
+
+            var relativeUri = fromUri.MakeRelativeUri(toUri);
+            var relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            return relativePath;
+        }
+    }
+}
